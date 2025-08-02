7533501f48d392e99ff227eda1897658edb389d7
Shorouk Amr <shoroukgnena@gmail.com>
2025-08-02 23:44:46 +0300

    [136] [Time Beats: 100.00%] [Memory Beats: 91.29%] - LeetPush

---
+++ Single-Number.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    public int SingleNumber(int[] nums) {
+        if(nums.Length == 1)
+            return nums[0];
+        
+        int result = nums[0];
+        for(int i=1; i<nums.Length; i++){
+            result ^= nums[i];
+        }
+
+        return result;
+    }
+}
